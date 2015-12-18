64dc77c4b9ef1c8872fdd5e7ca1eebfd32a2a9b0
malexander493 <malexander493@gmail.com>
2015-12-18 16:20:57 +0500

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -1,10 +1,7 @@
 @echo off
-:title           :startup.sh
+:title           :startup.bat
 :description     :Batch script to run Instagram Swiss Army Knife Project.
-:author		 	 :orbit software solutions
-:date            :2015-11-13
-:version         :1.0    
-:usage		 	 :startup.bat
+:author		 :orbit software solutions
+:usage		 :startup.bat
 :==============================================================================
-
 java -Xmx128m -jar ./target/isak-1.0-SNAPSHOT-jar-with-dependencies.jar %*
