7d54c9f4502417e3a7159b5cb292f218b2c79320
malexander493 <malexander493@gmail.com>
2015-12-29 15:30:47 +0500

    update typos

--- startup.bat
+++ startup.bat
@@ -1,10 +1,7 @@
 @echo off
-:title           :startup.sh
-:description     :Batch script to run Twitter Swiss Army Knife Project.
-:author		 	 :orbit software solutions
-:date            :2015-11-13
-:version         :1.0    
-:usage		 	 :startup.bat
+:title           :startup.bat
+:description     :Batch script to run Instagram Swiss Army Knife Project.
+:author		 :orbit software solutions
+:usage		 :startup.bat
 :==============================================================================
-
-java -Xmx128m -jar ./target/tsak-v1.0-jar-with-dependencies.jar %*
+java -Xmx128m -jar ./target/isak-1.0-SNAPSHOT-jar-with-dependencies.jar %*
--- startup.sh
+++ startup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #title           :startup.sh
-#description     :Shell script to run Twitter Swiss Army Knife Project.
-#author		 	 :orbit software solutions
-#date            :2015-11-10
-#version         :1.0    
-#usage		 	 :bash startup.sh
+#description     :Shell script to run Instagram Swiss Army Knife Project.
+#author		 :orbit software solutions
+#usage		 :bash startup.sh
 #==============================================================================
-java -Xmx128m -jar ./target/tsak-v1.0-jar-with-dependencies.jar $*
+java -Xmx128m -jar ./target/isak-1.0-SNAPSHOT-jar-with-dependencies.jar $*
